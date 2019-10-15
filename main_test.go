888fc62e141dbae1ca51b14e302af3d6bdd31850
ERTILU <rezaraka00@gmail.com>
2019-10-15 19:33:07 +0700

    add testing cover 34.5% of statements

--- main_test.go
+++ main_test.go
@@ -15,6 +15,42 @@ func TestCount(t *testing.T) {
 			t.Fatalf("return must be %s %s instead of Huruf mati: %s Huruf hidup: %s", expectedConsonant, expectedVowels, consonants, vowels)
 		}
 	})
+
+	// test is the input is no vowels
+	t.Run("No vowels input", func(t *testing.T) {
+		input := "thsnptsnvwls"
+		consonants, vowels := count(input)
+		expectedConsonant := "Huruf mati: 12,"
+		expectedVowels := "Huruf hidup: 0"
+		if consonants != expectedConsonant && vowels != expectedVowels {
+			t.Fatalf("return must be %s %s instead of Huruf mati: %s Huruf hidup: %s", expectedConsonant, expectedVowels, consonants, vowels)
+		}
+	})
+
+	// test is the input is no vowels
+	t.Run("No vowels input", func(t *testing.T) {
+		input := "thsnptsnvwls"
+		consonants, vowels := count(input)
+		expectedConsonant := "Huruf mati: 12,"
+		expectedVowels := "Huruf hidup: 0"
+		if consonants != expectedConsonant && vowels != expectedVowels {
+			t.Fatalf("return must be %s %s instead of Huruf mati: %s Huruf hidup: %s", expectedConsonant, expectedVowels, consonants, vowels)
+		}
+	})
+
+	// test is the input is no consonants
+	t.Run("No vowels input", func(t *testing.T) {
+		input := "aiueo"
+		consonants, vowels := count(input)
+		expectedConsonant := "Huruf mati: 0,"
+		expectedVowels := "Huruf hidup: 5"
+		if consonants != expectedConsonant && vowels != expectedVowels {
+			t.Fatalf("return must be %s %s instead of Huruf mati: %s Huruf hidup: %s", expectedConsonant, expectedVowels, consonants, vowels)
+		}
+	})
+
+	// call the main func so the coverage will 100% tested
+	main()
 }
 
 func BenchmarkCount(b *testing.B) {
